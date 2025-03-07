425ad96e79205ccae7f1f1faa875daeb9b05dab9
abdulhadii123 <hadi.woodenclouds@gmail.com>
2025-03-07 14:37:52 +0530

    latest change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Brands from '../components/Brands';
 import Projects from '../components/Projects';
 import Hero from '../components/Hero';
