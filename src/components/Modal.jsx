9eb2205d4fd6835504e8eb8ea67b165a65564bfb
abdulhadii123 <hadi.woodenclouds@gmail.com>
2025-03-07 14:57:09 +0530

    push

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -44,7 +44,7 @@ export default function Modal({ modalData }) {
                 {url && (
                   <li className="d-flex">
                     <span className="col-4 col-lg-3">Live URL:</span>
-                    <span><a href="">{url}</a></span>
+                    <span>{url}</span>
                   </li>
                 )}
               </ul>
